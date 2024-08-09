5319998c9b5159d4a20c12a33f381afa2fa37c7f
fibber2240 <96056157+fibber2240@users.noreply.github.com>
2024-08-09 12:26:44 +0300

    Работа со временем Использование классов LocalDate and LocalTime

--- src/Current.java
+++ src/Current.java
@@ -1,13 +1,17 @@
 import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
+import java.time.*;
 
 public class Current {
     public static void main(String[] args) {
-        Data current = new Data() {
-            @Override
-            public int hashCode() {
-                return super.hashCode();
-            }
-        };
-        System.out.println(current);
+//        Data current = new Data() {
+//            @Override
+//            public int hashCode() {
+//                return super.hashCode();
+//            }
+//        };
+//        System.out.println(current);
+        LocalTime currentLocalDateTime = LocalTime.now();
+        System.out.println(currentLocalDateTime);
     }
 }
